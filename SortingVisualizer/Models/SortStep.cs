6e9afc47339dc79af4d888cf478d3728676e2532
DOPIAS <dimabondar2006@gmail.com>
2026-06-16 17:11:58 +1000

    Create SortStep.cs
    Добавление модели данных: создан класс SortStep для отслеживания шагов сортировки

---
+++ SortingVisualizer/Models/SortStep.cs
@@ -0,0 +1,19 @@
+﻿namespace SortingVisualizer.Models
+{
+    // Описание, что делает алгоритм в данный момент
+    public enum StepType
+    {
+        Comparison, // Сравнивает
+        Swap,       // Меняет местами
+        Done        // Закончил работу
+    }
+
+    // Состояние массива на каждом шаге
+    public class SortStep
+    {
+        public StepType Type { get; set; }
+        public int Index1 { get; set; }
+        public int Index2 { get; set; }
+        public int[] CurrentArray { get; set; } = [];
+    }
+}
